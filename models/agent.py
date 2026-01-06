1cb0e7389aca5c8e75d13e870b7d38a71c41cc97
NEXT15930 <fake@email.com>
2026-01-06 11:25:49 +0100

    Create agent class and move method

---
+++ models/agent.py
@@ -0,0 +1,24 @@
+# Agent abstract class
+import random
+
+class Agent:
+    def __init__(self):
+        self.x = random.randrange(0, 100)
+        self.x = random.randrange(0, 100)
+        
+    def eat():
+        pass
+    def move():
+        self.x += random.randrange(-1, 1)
+        self.y += random.randrange(-1, 1)
+        # Bring the object back into the screen
+        if self.x > 100:
+            self.x -= 1
+        else if self.x < 0:
+            self.x += 1
+        if self.y > 100:
+            self.y -= 1
+        else if self.y < 0:
+            self.y += 1
+    def reproduce():
+        pass
