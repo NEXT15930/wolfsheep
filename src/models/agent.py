0674847d599e5318d49b274ff19990d51adab646
NEXT15930 <fake@email.com>
2026-01-20 11:20:14 +0100

    Make agent actually an abstract class

--- src/models/agent.py
+++ src/models/agent.py
@@ -1,41 +1,34 @@
 # Agent abstract class
 from .constants import *
+from abc import ABC, abstractmethod
 import random
 
-class Agent:
+class Agent(ABC):
     def __init__(self):
         self.x = random.randrange(0, WIDTH)
         self.y = random.randrange(0, HEIGHT)
         self.energy = ENERGY
         self.alive = True
-        
-    def eat():
-        pass
 
-    def move(self):
-        moves = [-1, 1]
-        if random.randint(0, 1) == 1:
-            self.x += random.choice(moves)
-        else:
-            self.y += random.choice(moves)
-        # Wrap around the screen
-        if self.x >= WIDTH:
-            self.x = 0
-        elif self.x < 0:
-            self.x = WIDTH-1
-        elif self.y >= HEIGHT:
-            self.y = 0
-        elif self.y < 0:
-            self.y = HEIGHT-1
-
-        self.energy -= 1
-        if self.energy <= 0:
-            self.die()
-        print(f"x: {self.x}\ny: {self.y}\nenergy: {self.energy}")
-        return self.x, self.y
+    @property
+    def x(self):
+        return self.x
+    
+    @property
+    def y(self):
+        return self.y
 
     def die(self):
         self.alive = False
+        
+    @abstractmethod
+    def move(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def interact(self):
+        raise NotImplementedError
     
+    @abstractmethod
     def reproduce():
-        pass
+        raise NotImplementedError
