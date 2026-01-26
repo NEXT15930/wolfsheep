fdb77c93d7fa10726cc7635ad0f8ba5952f1037d
NEXT15930 <fake@email.com>
2026-01-26 10:20:04 +0100

    Move doesn't need to be abstract

--- src/models/agent.py
+++ src/models/agent.py
@@ -22,9 +22,27 @@ class Agent(ABC):
     def die(self):
         self.alive = False
         
-    @abstractmethod
     def move(self):
-        raise NotImplementedError
+        moves = [-1, 1]
+        if random.randint(0, 1) == 1:
+            self.x += random.choice(moves)
+        else:
+            self.y += random.choice(moves)
+        # Wrap around the screen
+        if self.x >= WIDTH:
+            self.x = 0
+        elif self.x < 0:
+            self.x = WIDTH-1
+        elif self.y >= HEIGHT:
+            self.y = 0
+        elif self.y < 0:
+            self.y = HEIGHT-1
+
+        self.energy -= 1
+        if self.energy <= 0:
+            self.die()
+        print(f"x: {self.x}\ny: {self.y}\nenergy: {self.energy}")
+        return self.x, self.y
 
     @abstractmethod
     def interact(self):
