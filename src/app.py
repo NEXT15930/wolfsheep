b2e54d899b829b8a88bed887344698fe046a0a80
NEXT15930 <fake@email.com>
2026-02-05 19:11:26 +0100

    Animation

--- src/app.py
+++ src/app.py
@@ -2,35 +2,49 @@ from model import Model
 from models.prey import Prey
 from models.predator import Predator
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 PREY = 10
 PREDATOR = 4
-STEPS = 30
+STEPS = 300
 
-def main():
-    model = Model(seed=2)
-    for i in range(PREY):
-        model.add_agent(Prey(model))
-    for i in range(PREDATOR):
-        model.add_agent(Predator(model))
+model = Model(seed=2)
+# Data variables
+time = []
+prey_count = []
+predator_count = []
 
-    # Data variables
-    time = []
-    prey_count = []
-    predator_count = []
-    
-    # Loop
-    for i in range(STEPS):
-        model.step()
-        # Data collection
-        prey_count.append(model.get_prey())
-        predator_count.append(model.get_predator())
-    # TODO: plot data
-    print(prey_count)
-    print(predator_count)
-    plt.plot(prey_count)
-    plt.plot(predator_count)
-    plt.show()
+# Setup plot
+fig, ax = plt.subplots()
+line, = ax.plot([], [], label = "Prey count")
+line2, = ax.plot([], [], label = "Predator count")
+ax.legend()
+ax.set_title("ABM: Prey vs Predator")
+ax.set_xlabel("Time")
+ax.set_ylim(0, 150)
+ax.set_xlim(0, STEPS)
 
-main()
-        
+def update(frame):
+    global ani
+    if frame >= STEPS-1:
+        ani.pause()
+    model.step()
+    # Data collection
+    time.append(model.time)
+    prey_count.append(model.get_prey())
+    predator_count.append(model.get_predator())
+    line.set_xdata(time)
+    line.set_ydata(prey_count)
+    line2.set_xdata(time)
+    line2.set_ydata(predator_count)
+    return line, line2
+
+# Add agents to model
+for i in range(PREY):
+    model.add_agent(Prey(model))
+for i in range(PREDATOR):
+    model.add_agent(Predator(model))
+
+# Animation
+ani = FuncAnimation(fig, update, frames = STEPS, interval = 50)
+plt.show()
